785c5385d7c07f824119fc66b5db9912d25c160d
Brian Chou <brianchou_5@hotmail.com>
2022-10-17 22:35:20 +0800

    Added Day1 files

---
+++ day-1-printing-start/main.py
@@ -0,0 +1,4 @@
+# Write your code below this line 👇
+bandFirstName = input("What's name of the city you grew up in?\n")
+bandLastName = input("What's your pet's name?\n")
+print("Your band name could be " + bandFirstName + " " + bandLastName +".")
